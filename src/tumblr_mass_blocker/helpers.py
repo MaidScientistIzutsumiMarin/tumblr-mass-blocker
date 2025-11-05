b3329bde25a7a90363ff6aea45b72b8b5c6e82b2
MaidScientistIzutsumiMarin <>
2025-11-05 03:21:50 -0500

    Add comment to helpers

--- src/tumblr_mass_blocker/helpers.py
+++ src/tumblr_mass_blocker/helpers.py
@@ -2,5 +2,7 @@ from nicegui.binding import BindableProperty
 from nicegui.ui import input as ui_input
 
 
+# This is a slightly hacky way to make nearly any attribute of any class bindable.
+# Bindable properties are far more efficient than active links, which is the alternative.
 class BindableInput(ui_input):
     _error = BindableProperty()  # pyright: ignore[reportAssignmentType]
